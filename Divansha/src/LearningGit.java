9105e319bd82c41b08c4c8bfe1aa5be00538dcf2
kasumala <kasumala@LIN80027371.corp.capgemini.com>
2024-02-13 11:11:02 +0530

    Adding Numbers

--- Divansha/src/LearningGit.java
+++ Divansha/src/LearningGit.java
@@ -1,7 +1,18 @@
 
 public class LearningGit {
 
-	public static void main(String[] args) {
+	    public static void main(String[] args) {
+
+	        char ch = 'a';
+	        int ascii = ch;
+	        // You can also cast char to int
+	        int castAscii = (int) ch;
+
+	        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+	        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+	    }
+	
+	public static void main1(String[] args) {
 		// TODO Auto-generated method stub
  System.out.println("Leaning Git");
 	}
